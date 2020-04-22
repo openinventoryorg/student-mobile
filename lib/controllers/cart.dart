bc3ee197ffae1dfdccc9257de6daa707afc3e5bc
K. D. Sunera Avinash Chandrasiri <kdsuneraavinash@gmail.com>
2020-04-23 01:57:43 +0530

    Added some comments

--- lib/controllers/cart.dart
+++ lib/controllers/cart.dart
@@ -7,14 +7,22 @@ class CartController extends ChangeNotifier {
   static const CART_LIMIT = 4;
   final Map<String, Set<CartItem>> cart = {};
 
+  /// Helper method to get controller by context
   static CartController of(BuildContext context) {
     return Provider.of<CartController>(context, listen: false);
   }
 
+  /// Helper method to get controller by context
+  ///
+  /// This also starts litening for future changes
   static CartController listenOf(BuildContext context) {
     return Provider.of<CartController>(context, listen: true);
   }
 
+  /// Given a cart id, add an item
+  ///
+  /// Howvever if cart id is not defined, the cart is then created.
+  /// Cart max space req are also ensured
   void addItem(String cartId, CartItem item) {
     if (!cart.containsKey(cartId)) {
       cart[cartId] = Set();
@@ -26,6 +34,7 @@ class CartController extends ChangeNotifier {
     notifyListeners();
   }
 
+  /// Removes a item from cart if the cart and item exists
   void removeItem(String cartId, CartItem item) {
     if (!cart.containsKey(cartId)) {
       return;
@@ -37,6 +46,7 @@ class CartController extends ChangeNotifier {
     notifyListeners();
   }
 
+  /// Getter for cart items in a cart
   Set<CartItem> getCartItems(String cartId) {
     if (!cart.containsKey(cartId)) {
       cart[cartId] = Set();
@@ -44,6 +54,7 @@ class CartController extends ChangeNotifier {
     return cart[cartId];
   }
 
+  /// Checks if the item is in the cart
   bool isInCart(String cartId, CartItem itemId) {
     if (!cart.containsKey(cartId)) {
       return false;
@@ -52,6 +63,7 @@ class CartController extends ChangeNotifier {
   }
 }
 
+/// Data class with operators overloaded to be used as cart item
 class CartItem {
   final String id;
   final String title;
